270c10cff9164ed7a958939af5ebe460ecf240a5
hamid <hamidghahremani2001@gmail.com>
2025-10-05 00:49:06 +0330

    in Base Model: 'id' -> 'ID' (avoid conflicts) + some changes in column's definition

--- src/models/base.py
+++ src/models/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy import BigInteger
+from sqlalchemy import BigInteger, Identity
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs
 
@@ -6,7 +6,11 @@ from sqlalchemy.ext.asyncio import AsyncAttrs
 class Base(AsyncAttrs, DeclarativeBase):
     """ BaseClass for declaring other tables """
     # same ID for all Tables
-    # for Tables with different ID: you can override 'id' attribute
-    id: Mapped[int] = mapped_column(
-        BigInteger, primary_key=True, autoincrement=True, index=True,
-    )  # NOTE: 'nullable=False' & 'unique=True' are automatically applied.
+    # for Tables with different ID: you can override 'ID' attribute
+    ID: Mapped[int] = mapped_column(
+        BigInteger, Identity(start=1, increment=1), primary_key=True
+    )
+    # NOTE:
+    # no need for:
+    # _ nullable=False, unique=True, autoincrement=True -> Identity(...)
+    # _ index=True -> Postgres automatically indexes Primary Key
